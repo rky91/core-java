cb41933730e493c5a2a75b125bb6af3e5bab0cc0
rky91 <rakeshkumar.jena91@gmail.com>
2019-01-27 17:46:51 +0530

    .

--- CoreJavaPractice/src/com/corejava/collections/compare/App.java
+++ CoreJavaPractice/src/com/corejava/collections/compare/App.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		
 		List<Employee> empList = new ArrayList<>();
 		empList = Arrays.asList(new Employee("Rakesh", "Jena", 26, 40000L),
