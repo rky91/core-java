dafc7d12c0f3b1fa290e92931a66b542caead84d
rky91 <rakeshkumar.jena91@gmail.com>
2019-01-27 21:24:16 +0530

    Connection pool

---
+++ design-demo/src/com/corejava/bt/demo/db/DataSourceUtil.java
@@ -0,0 +1,35 @@
+package com.corejava.bt.demo.db;
+
+import javax.sql.DataSource;
+
+public class DataSourceUtil {
+	
+	private static DataSourceUtil dataSourceUtilobj = null;
+	private ConnectionPool jdbcObj = new ConnectionPool();
+	
+	public static DataSourceUtil getInstance() {
+		if(dataSourceUtilobj != null) {
+			return dataSourceUtilobj;
+		} else {
+			return new DataSourceUtil();
+		}
+	}
+	
+	//setup the connection pool
+	public DataSource getPoolDataSource() {
+		DataSource dataSource = null;
+		try {
+			dataSource = jdbcObj.setUpPool();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dataSource;
+	}
+	
+	//print DB status
+	public void printDbStatus() {
+		jdbcObj.printDbStatus();
+	}
+	
+	
+}
---
+++ design-demo/src/com/corejava/bt/demo/db/MainApp.java
@@ -0,0 +1,57 @@
+package com.corejava.bt.demo.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ResultSet rsObj = null;
+		Connection connObj = null;
+		PreparedStatement pstmtObj = null;
+		//ConnectionPool jdbcObj = new ConnectionPool();
+		
+		try {
+
+			// Performing Database Operation!
+			System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+			connObj = DataSourceUtil.getInstance().getPoolDataSource().getConnection();
+			DataSourceUtil.getInstance().printDbStatus();
+			
+			pstmtObj = connObj.prepareStatement("select * from cube_dev.price");
+			rsObj = pstmtObj.executeQuery();
+			while (rsObj.next()) {
+				System.out.println("Price: " + rsObj.getString("price"));
+			}
+			System.out.println("\n=====Releasing Connection Object To Pool=====\n");
+			DataSourceUtil.getInstance().printDbStatus();
+			
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				// Closing ResultSet Object
+				if(rsObj != null) {
+					rsObj.close();
+				}
+				// Closing PreparedStatement Object
+				if(pstmtObj != null) {
+					pstmtObj.close();
+				}
+				// Closing Connection Object
+				if(connObj != null) {
+					connObj.close();
+				}
+			} catch(Exception sqlException) {
+				sqlException.printStackTrace();
+			}
+		}
+		DataSourceUtil.getInstance().printDbStatus();
+
+	}
+
+}
